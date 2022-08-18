822d8c611b1dc40b0250be79daf6f32f2591bc7d
Harry Chappell <harry@hcdigital.co.uk>
2022-08-18 13:13:28 +0100

    Adding fonts and setting breakpoints

--- page-form.php
+++ page-form.php
@@ -17,11 +17,40 @@
 
 <div id="content">
 
-	<div id="inner-content" class="wrap cf">
+	<div id="inner-content" class="">
 
 		<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-			<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+			<h1 class="page-title wrap" itemprop="headline"><?php the_title(); ?></h1>
+
+			<form action="">
+
+				<div id="who-are-you" 	class="wrap">
+					<h2 class="font-prisma">Who are you?</h2>
+				</div>
+
+				<div id="contact" 		class="wrap">
+					<h2 class="font-abril-fatface">How do we contact you?</h2>
+				</div>
+
+				<div id="sport-shirt" 	class="wrap">
+					<h2 class="font-rush-brush">What's your desired sport and shirt number?</h2>
+				</div>
+
+				<div id="audio" 		class="wrap">
+					<h2 class="font-leira">What're your audio prefrences?</h2>
+				</div>
+
+				<div id="turtles" 		class="wrap">
+					<h2 class="font-awery">Which is the best turtle?</h2>
+				</div>
+
+				<div id="wakeup-alarm" 	class="wrap">
+					<h2 class="font-krungthep">When would you like your wakeup alarm set?</h2>
+				</div>
+
+				<button class="submit font-budmo-jigglish">Submit</button>
+			</form>
 
 		</main>
 
